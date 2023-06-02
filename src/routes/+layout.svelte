3b48b44939b3d7866e6d64c928dc9ee868ebcd0f
Jimmi Mortensen <52856973+thatjimmi@users.noreply.github.com>
2023-06-02 22:50:03 +0200

    PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,6 +28,14 @@
     href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap"
     rel="stylesheet"
   />
+
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="manifest" href="/site.webmanifest" />
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+  <meta name="msapplication-TileColor" content="#da532c" />
+  <meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
 <slot />
